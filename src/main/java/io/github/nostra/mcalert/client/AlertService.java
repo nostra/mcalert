2985ea886b636b0f0959d74608aad066b225fb05
Erlend Nossum <nossum@gmail.com>
2024-02-09 08:49:31 +0100

    Naming config for clarity

--- src/main/java/io/github/nostra/mcalert/client/AlertService.java
+++ src/main/java/io/github/nostra/mcalert/client/AlertService.java
@@ -3,15 +3,13 @@ package io.github.nostra.mcalert.client;
 import io.github.nostra.mcalert.model.PrometheusResult;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@RegisterRestClient
+@RegisterRestClient(configKey = "single-client")
 public interface AlertService {
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
-    @Path("/alerts")
     PrometheusResult getResult();
 
 }
